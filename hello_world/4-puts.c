ec37d80761b882ea6e66e922cd8dae28c3900d4b
NickelannG <8836@holbertonschool.comn>
2024-02-20 06:43:07 +0000

    fix: filename

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,6 @@
-#!/bin/bash
-gcc puts("Programming is like building a multilingual puzzle")
-return 0;
+#include <4-puts.c>
+
+int main () {
+    puts("Programming is like building a multilingual puzzle"); /* This will print the quote followed by a newline */
+    return 0;
+}    
