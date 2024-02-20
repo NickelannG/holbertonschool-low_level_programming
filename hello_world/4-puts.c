2b49a8799579d047666702f09a1afc5c27b77cf5
NickelannG <8836@holbertonschool.comn>
2024-02-20 06:55:34 +0000

    fix: added more comments

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+/* file name 4-puts.c */
 
-int main () {
-    puts("Programming is like building a multilingual puzzle"); /* This will print the quote followed by a newline */
-    return 0;
+#include <stdio.h>
+/**
+ * main will print "Programming is like building a multilingual puzzle", followed by a new line
+ *
+ * program will end with the value 0
+ */
+
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle");
+
+  return 0;
 }    
