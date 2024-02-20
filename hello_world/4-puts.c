15502398813bb4b01e6b55849b3feb9ba36ef18d
NickelannG <8836@holbertonstudents.com>
2024-02-20 09:14:22 +0000

    betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main will print "Programming is like building a multilingual puzzle",
+ * main - print "Programming is like building a multilingual puzzle",
  * followed by a new line
  *
- * program will end with the value 0
+ * Return: 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
