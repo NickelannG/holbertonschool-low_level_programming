a1d7905a5954a8453c1a7fc2e1e388dc05ad365a
NickelannG <8836@holbertonschool.com>
2024-02-22 08:48:28 +0000

    fix clean code than is betty compatible

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/**                                                                                                                              
- * main - a program that prints the last digit of a random number that is                                                        
- * stored in the variable n followed by a newline                                                                                
- *                                                                                                                               
- * Return: always 0                                                                                                              
+/**
+ * main - a program that prints the last digit of a random number and tells
+ * you if its greater than 5, 0, or less than 6 followed by a new line
+ *
+ * Return: always 0
  */
+
 int main(void)
 {
 	int n, last_digit;
@@ -18,10 +18,10 @@ int main(void)
 
 	printf("Last digit of %d is %d ", n, last_digit);
 	if (last_digit > 5)
-        	printf("and is greater than 5\n");
+		printf("and is greater than 5\n");
 	else if (last_digit == 0)
-        	printf("and is 0\n");
+		printf("and is 0\n");
 	else
-        	printf("and is less than 6 and not 0\n");
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
