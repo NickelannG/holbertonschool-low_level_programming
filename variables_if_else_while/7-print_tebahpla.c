127ef2ddf47e79573c177909d4723004bb2846f3
NickelannG <8836@holbertonstudents.com>
2024-02-22 22:38:06 +0000

    smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ * followed by a new line
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch <= 'a')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
