d81a0d0b3e1e896280f1439d3496d4e4c14aebf9
NickelannG <8836@holbertonstudents.com>
2024-02-23 02:16:34 +0000

    patientce task 9 fix to print correct output

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,11 +12,12 @@ int main(void)
 	for (n = 0; n < 10 ; n++)
 	{
 		putchar(n + '0');
-		if (n != 9)
+		if (n < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('$');
 	return (0);
 }
