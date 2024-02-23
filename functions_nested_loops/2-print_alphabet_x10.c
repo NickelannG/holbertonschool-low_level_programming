38634c6ebdcb52a2f7960bbd752c07d6aba2da78
NickelannG <8836@holbertonstudents.com>
2024-02-23 11:19:06 +0000

    10x alphabet task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch = 'a';
-
-	while (ch <= 'z')
+	for (int i = 0; i < 10; i++)
 	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
 		_putchar(ch);
 		ch++;
+		}
 	}
 	_putchar('\n');
 }
