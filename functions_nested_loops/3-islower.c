64d3a8048d3b870f5229363453626ce13d417a03
NickelannG <8836@holbetonstudents.com>
2024-02-24 05:50:37 +0000

    islower task 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -8,12 +8,11 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122) /* 97 in ASCII is a, and 122 is z */
 	{
-		_putchar(1);
+		return (1);
 	}
 	else
 	{
-		_putchar(0);
+		return (0);
 	}
 	_putchar('\n');
-	return (0);
 }
