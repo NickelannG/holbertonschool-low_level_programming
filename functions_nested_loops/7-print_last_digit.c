e9021b1aba288fe62942b7571964b888669bb521
NickelannG <8836@holbetonstudents.com>
2024-02-24 09:10:56 +0000

    style: removed last_digit to make code simple

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,13 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit;
-
 	if (n < 0)
-		last_digit = -n % 10;
+	{
+		_putchar((-n % 10) + 0);
+	}
 	else
-		last_digit = n % 10;
-
-	_putchar(last_digit + '0');
-	return (last_digit);
+	{
+		_putchar((n % 10) + 0);
+	}
+	_putchar('\n');
 }
