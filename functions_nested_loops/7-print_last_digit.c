6d568fb5b8d0530ae36d62dcbfa2324eedd22e77
NickelannG <8836@holbetonstudents.com>
2024-02-24 08:31:09 +0000

    fix: removed _putchar for stdout to be 3 and not 4

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,13 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-		n = -n;
-	}
-	int last_digit = n % 10;
+	int last_digit;
 
+	if (n < 0)
+		last_digit = -n % 10;
+	else
+		last_digit = n % 10;
+
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
