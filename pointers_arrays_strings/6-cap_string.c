83d830f7df34e07edd569442e8b95efea5e49327
NickelannG <8836@holbertonstudents.com>
2024-03-01 23:49:06 +0000

    Task 6: expect the best

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * cap_string - a functioon that capitalizes all words of a string
+ *
+ * @str: string to be modified
+ *
+ * Retunr: modified string
+ */
+char *cap_string(char *)
+{
+	char *ptr = str; /* Initialise pointer to a string */
+
+	while (*ptr != '0') /* loop through string until null */
+	{
+		if (*ptr >= 97 && *ptr <= 122 ||
+}
