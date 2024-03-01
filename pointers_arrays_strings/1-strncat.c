7958474faef3b50d8461739b7f451459c2b2c52f
NickelannG <8836@holbertonstudents.com>
2024-03-01 12:37:52 +0000

    Task 1: strncat draft

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - A function that concatenates two strings
+ *
+ * @dest: a pointer to the beginning of the first string
+ * @src: a pointer to the beginning of the second string
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+}
