44d4bd151b9e6ac3d7e17f4a6a59f0e1ea20a6e8
NickelannG <8836@holbertonstudents.com>
2024-03-01 09:02:20 +0000

    Task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,34 +9,38 @@
  */
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int size = 0;
-	int counter = 0;
-	int i;
-	int m = 1;
+	int ii = 0, num = 0;
+	int start = -1;
+	int end = -1;
+	int mult = 1;
 
-	while (*(s + counter) != '\0')
+	while (start < 0)
 	{
-		if (size > 0 && (*(s + counter) < '0' || *(s + counter) > '9'))
-			break;
-
-		if (*(s +  counter) == '-')
-			sign *= -1;
-
-		if ((*(s + counter) >= '0') && (*(s + counter) <= '9'))
-		{
-			if (size > 0)
-				m *= 10;
-			size++;
-		}
-		counter++;
+		if (s[ii] == 0)
+			return (0);
+		if (s[ii] >= 48 && s[ii] <= 57)
+			start = ii;
+		else
+			ii += 1;
+	}
+	while (end < 0)
+	{
+		if (s[ii] < 48 || s[ii] > 57)
+			end = ii - 1;
+		else
+			ii += 1;
+	}
+	for (ii = start - 1 ; ii >= 0 ; ii--)
+	{
+		if (s[ii] == 45)
+			mult = mult * (-1);
+	}
+	for (ii = end ; ii >= start ; ii--)
+	{
+		num += (s[ii] - 48) * mult;
+		if (ii != start)
+			mult *= 10;
 	}
 
-	for (i = counter - size; i < counter; i++)
-	{
-		result = result + ((*(s + i) - 48) * m);
-		m /= 10;
-	}
-	return (result * sign);
+	return (num);
 }
