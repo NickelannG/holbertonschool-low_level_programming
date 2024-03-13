b284c5f6856b733af74e5d0bba36652a970deae5
NickelannG <8836@holbertonstudents.com>
2024-03-13 09:12:24 +0000

    task 0: poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - to show it's name, age and owner
+ *
+ * @name: a pointer to a character array representing dog's name
+ * @age: a floating-point value to represent dog's age
+ * @owner: a pointer to a character array representing do's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
