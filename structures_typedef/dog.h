2bb47d195fd1d92f3fa9f5ccc853fa3b6ddef497
NickelannG <8836@holbertonstudents.com>
2024-03-15 02:58:04 +0000

    Task 3: Outside of a dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,12 +9,12 @@
  * @owner: a pointer to a character array representing do's owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
