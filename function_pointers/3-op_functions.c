68b73b39b4bee5c88c6e182ad8cfb437d877afd1
NickelannG <8836@holbertonstudents.com>
2024-03-19 04:16:08 +0000

    Task 3 header file and op functions file

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+}
+
+/**
+ * op_sub - subtracts one integer from another
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+}
+
+/**
+ * op_div - divides one integer by another
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+}
+
+/**
+ * op_mod - divides one integer by another
+ * @a: first integer
+ * @b: second integer
+ * Return: the remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+}
