61f30baf34edaa711e0d726bcb890eca3abc257d
NickelannG <8836@holbertonstudents.com>
2024-03-20 00:48:32 +0000

    Task 3 docs: betty

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -13,14 +13,13 @@
 int (*get_op_func(char *s))(int, int)
 {
 	/* Define array of structs */
-	op_t ops[] = 
-	{
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
 	};
 
 	int i = 0;
