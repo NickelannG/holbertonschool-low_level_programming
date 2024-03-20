ded50a955dcfc671f48ee995c2bc055a5f359bda
NickelannG <8836@holbertonstudents.com>
2024-03-20 03:58:10 +0000

    Task 3 docs: betty

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 
 
-	if (atoi(argv[3]) == 0 && (*argv[2] == '/' || *argv[2] == '%'))	
+	if (atoi(argv[3]) == 0 && (*argv[2] == '/' || *argv[2] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
