a567ab1f09f0929b11e18649aaa7b8db6cbd0b9f
NickelannG <8836@holbertonstudents.com>
2024-03-22 06:42:33 +0000

    Task 0: Beauty is variable

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - a function that returns the sum of all its parameters
+ *
+ * @n: number of parameters
+ *
+ * Return: 0, if n is 0 otherwise, the sum of all the parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int s = 0;
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		s += va_arg(args, int);
+	}
+	va_end(args);
+
+	return (s);
+}
