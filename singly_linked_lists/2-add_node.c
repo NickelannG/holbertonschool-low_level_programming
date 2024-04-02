5004fa802264a736e822888753fda0fc991546b3
NickelannG <8836@holbertonstudents.com>
2024-04-02 02:02:05 +0000

    task 2: add new node to head of linked list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * add_node - adds a new node at the beginning of a linked list list_t
+ *
+ * @head: a pointer to a pointer to the head of the linked list
+ * @str: a pointer to a string to be duplicated and stored into the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(new_node));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (*head);
+}
+
+/**
+ * free_list - dellocate memory for new node place at the
+ * beginning of linked list
+ *
+ * @head: a pointer to the head of the linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
