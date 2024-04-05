f28cfffcc1b9bba489c6ba6ad329c88ea51c5794
NickelannG <8836@holbertonstudents.com>
2024-04-05 03:57:24 +0000

    task 1: print binary representation of a number

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ *
+ * @n: the number
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	int num_bits, i, bit;
+	int leading_zero = 1;
+	
+	/* to get total number of bits for unsigned long int */
+	num_bits = sizeof(unsigned long int) * 8;
+	
+	/* start from MSB */
+	i = num_bits - 1;
+
+	/* iterate through n to find first non-zero bit */
+	while (i >= 0 && ((n >> i) & 1) == 0)
+		i--;
+
+	while (i >= 0)
+	{	/* i-th bit of n */
+		bit = (n >> i) & 1;
+
+		if (bit)
+			leading_zero = 0;
+		if (!leading_zero || i == 0)
+			_putchar(bit +'0');
+		i--;
+	}
+}
