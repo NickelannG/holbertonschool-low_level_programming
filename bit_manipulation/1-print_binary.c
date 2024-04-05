2eab62f26696fc13b783a6755ff07ffa576cd2d3
NickelannG <8836@holbertonstudents.com>
2024-04-05 04:00:30 +0000

    task 1 fix: include case where n is 0

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -11,16 +11,19 @@ void print_binary(unsigned long int n)
 {
 	int num_bits, i, bit;
 	int leading_zero = 1;
-	
+
 	/* to get total number of bits for unsigned long int */
 	num_bits = sizeof(unsigned long int) * 8;
-	
+
 	/* start from MSB */
 	i = num_bits - 1;
 
 	/* iterate through n to find first non-zero bit */
 	while (i >= 0 && ((n >> i) & 1) == 0)
 		i--;
+
+	if (i < 0)
+		leading_zero = 0;
 
 	while (i >= 0)
 	{	/* i-th bit of n */
@@ -29,7 +32,7 @@ void print_binary(unsigned long int n)
 		if (bit)
 			leading_zero = 0;
 		if (!leading_zero || i == 0)
-			_putchar(bit +'0');
+			_putchar(bit + '0');
 		i--;
 	}
 }
