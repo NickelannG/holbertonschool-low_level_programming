14b6dc2e9ff8d46a77695f30d9213c334358fbee
NickelannG <8836@holbertonstudents.com>
2024-04-05 01:05:38 +0000

    task 0: turn binary number to unsigned int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,3 +10,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int i = 0;
+	unsigned int sum = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		else
+		{
+			sum = sum*2 + (b[i] - '0');
+			i++;
+		}
+	}
+	return (sum);
+}
