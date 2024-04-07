e248304ac9364a0cb6511fb7bf3bbf7ea697cd72
NickelannG <8836@holbertonstudents.com>
2024-04-07 02:00:22 +0000

    Task 3 docs: betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,6 +1,47 @@
 #include "main.h"
 
 /**
+ * open_source_file - opens the source file
+ * @filename: the name of the file to open
+ *
+ * Return: the file descriptor of the opened file
+ */
+
+int open_source_file(const char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	return (fd);
+}
+
+/**
+ * create_dest_file - creates the destination file
+ * @filename: the name of the file to be created
+ *
+ * Return: the file descriptor of the created file
+ */
+
+int create_dest_file(const char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
+				filename);
+		exit(99);
+	}
+	return (fd);
+}
+
+/**
  * main - Entry point of thr program
  *
  * @argc: Number of arguments
@@ -20,19 +61,9 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "usage: cp file_from fileto\n");
 		exit(97);
 	}
-	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Can't write to file %s\n", argv[2]);
-		close(fd1);
-		exit(99);
-	}
+	fd1 = open_source_file(argv[1]);
+	fd2 = create_dest_file(argv[2]);
+
 	while ((bytesread = read(fd1, buffer, 1024)) > 0)
 	{
 		byteswritten = write(fd2, buffer, bytesread);
