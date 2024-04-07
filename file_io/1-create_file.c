f9bf1b63f58e33295063a86a42513a0b25767313
NickelannG <8836@holbertonstudents.com>
2024-04-07 00:14:29 +0000

    task 1 docs: betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -28,7 +28,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		byteswritten = write(fd, text_content, strlen(text_content));
-		if (byteswritten == -1 || (size_t)byteswritten != strlen(text_content)) /* If write fails */
+		/* If write fails */
+		if (byteswritten == -1 || (size_t)byteswritten != strlen(text_content))
 		{
 			close(fd);
 			return (-1);
